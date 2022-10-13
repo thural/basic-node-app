877ca2424f117d12d5316b1bb56e7fda256fe3f4
Tural Musaibov <tural.musaibov@gmail.com>
2022-10-13 18:23:21 +0400

    create funcs to load requested html pages

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
-const http = require('http')
+const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') res.end('Welcome to our home page')
-  else if (req.url === '/about') res.end('Here is our short history')
-  else {
-    res.end(`
-    <h1>Oops!</h1>
-    <p>We can't seem to find the page you are looking for</p>
-    <a href="/">back home</a>
-    `)
+const server = http.createServer((req,res)=>{
+  if(req.url==='/'){
+      fs.readFile('index.html',function(err,data){
+          if (err) throw err;
+          res.writeHead(200,{'Content-Type':'text/html'});
+          res.write(data);
+          return res.end;
+      });
+  }
+  else if(req.url==='/about'){
+      fs.readFile('about.html',function(err,data){
+          if (err) throw err;
+          res.writeHead(200,{'Content-Type':'text/html'});
+          res.write(data);
+          return res.end;
+      });
+  }
+  else if(req.url==='/contact'){
+      fs.readFile('contact.html',function(err,data){
+          if (err) throw err;
+          res.writeHead(200,{'Content-Type':'text/html'});
+          res.write(data);
+          return res.end;
+      });
+  }
+  else{
+      fs.readFile('404.html',function(err,data){
+          if (err) throw err;
+          res.writeHead(200,{'Content-Type':'text/html'});
+          res.write(data);
+          return res.end;
+      });
   }
 })
 
