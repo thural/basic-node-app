ed0cd9886f152b3740906a64e6bcf352d7b6552a
Tural Musaibov <tural.musaibov@gmail.com>
2022-10-13 20:33:36 +0400

    refactor readFile promise into an async function

--- index.js
+++ index.js
@@ -23,7 +23,24 @@ const { readFile } = require('fs').promises;
 //   }
 // };
 
+const startLoad = async (req, res, filePath) => {
+  try {
+    const data = await readFile(filePath, 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end
+  } catch (error) {
+    console.log(error);
+    const data = await readFile('./404.html', 'utf-8');
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end
+  }
+};
+
 const server = http.createServer((req, res) => {
+
+  const filePath = (req.url === '/' ? './index.html' : `.${req.url}.html`);
 
   // //using a custom promise
   // loadPage(req.url === '/' ? './index.html' : `.${req.url}.html`)
@@ -37,20 +54,26 @@ const server = http.createServer((req, res) => {
   //     res.end
   //   });
 
-  // startLoad(req, res, (req.url === '/' ? './index.html' : `.${req.url}.html`))
+  //using one liner async await func
+  startLoad(req, res, filePath);
 
+  // // using a built in promise from fs promises
+  // readFile(filePath, 'utf-8')
+  //   .then(data => {
+  //     res.writeHead(200, { 'Content-Type': 'text/html' });
+  //     res.write(data);
+  //     return res.end
+  //   })
+  //   .catch(error => {
 
-  // using a built in promise from fs promises
-  readFile((req.url === '/' ? './index.html' : `.${req.url}.html`), 'utf-8')
-    .then(data => {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.write(data);
-      return res.end
-    })
-    .catch(error => {
-      console.log(error);
-      res.end
-    });
+  //     console.log(error);
+
+  //     res.end(`
+  //   <h1>Oops!</h1>
+  //   <p>We can't seem to find the page you are looking for</p>
+  //   <a href="/">back home</a>
+  //   `)
+  //   });
 
   res.end
 
