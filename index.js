da1539dc564a777450d25161213c6aadec762b7f
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-19 22:30:31 +0400

    add support for styling and other files

--- index.js
+++ index.js
@@ -34,8 +34,36 @@ const { readFile } = require('fs');
 
 const server = http.createServer((req, res) => {
 
-  const filePath = (req.url === '/' ? './index.html' : `.${req.url}.html`);
+  const filePath = (req.url === '/' ? './index.html' : `.${req.url}`);
 
+  // Extension of file
+  let extname = filePath.match(/\.\w+$/)[0] // regex is good :)
+
+  // Initial content type
+  let contentType = "text/html";
+
+  // Check ext and set content type
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
+  }
+  console.log('filepath: ', filePath);
+  console.log('extname: ', extname);
+  console.log('contentType: ', contentType);
+  
   // //using one liner async await func
   // startLoad(res, filePath);
 
@@ -44,14 +72,14 @@ const server = http.createServer((req, res) => {
     if (error) {
       if (error.code == "ENOENT") {
         readFile('./404.html', 'utf-8', (error, content) => {
-            res.writeHead(404, { "Content-Type": "text/html" });
-            res.end(content);
-          })
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end(content);
+        })
       }
       res.writeHead(500);
       res.end(`Server Error: ${error.code}`);
     }
-    res.writeHead(200, { "Content-Type": "text/html" });
+    res.writeHead(200, { "Content-Type": contentType });
     res.end(data);
   });
 
