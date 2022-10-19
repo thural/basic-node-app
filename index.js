4f4e31a6362a1d55ba75b329eec8e3a09963fb1e
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-19 20:58:06 +0400

    use callback method

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const http = require('http');
-const { readFile } = require('fs').promises;
+const { readFile } = require('fs');
 
-// // create load page promise
-// const loadPage = (path) => {
+// const errorMsg = (
+//   `
+//   <h1>Oops!</h1>
+//   <p>We can't seem to find the page you are looking for</p>
+//   <a href="/">back home</a>
+//   `
+// );
+
+// // create a custom promise
+// const readFilePromise = (path) => {
 //   return new Promise((resolve, reject) => {
 //     readFile(path, 'utf-8', (error, data) => {
 //       if (error) reject(error)
@@ -11,77 +19,63 @@ const { readFile } = require('fs').promises;
 //   })
 // };
 
-// const startLoad = async (req, res, path) => {
+// const startLoad = async (res, filePath) => {
 //   try {
-//     const data = await loadPage(path);
+//     const data = await readFile(filePath, 'utf-8');
 //     res.writeHead(200, { 'Content-Type': 'text/html' });
-//     res.write(data);
-//     return res.end
+//     res.end(data);
 //   } catch (error) {
 //     console.log(error);
-//     res.end
+//     const data = await readFile('./404.html', 'utf-8');
+//     res.writeHead(200, { 'Content-Type': 'text/html' });
+//     res.end(data);
 //   }
 // };
-
-const startLoad = async (req, res, filePath) => {
-  try {
-    const data = await readFile(filePath, 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end
-  } catch (error) {
-    console.log(error);
-    const data = await readFile('./404.html', 'utf-8');
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write(data);
-    return res.end
-  }
-};
 
 const server = http.createServer((req, res) => {
 
   const filePath = (req.url === '/' ? './index.html' : `.${req.url}.html`);
 
+  // //using one liner async await func
+  // startLoad(res, filePath);
+
+  //using a callback function
+  readFile(filePath, (error, data) => {
+    if (error) {
+      if (error.code == "ENOENT") {
+        readFile('./404.html', 'utf-8', (error, content) => {
+            res.writeHead(404, { "Content-Type": "text/html" });
+            res.end(content);
+          })
+      }
+      res.writeHead(500);
+      res.end(`Server Error: ${error.code}`);
+    }
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(data);
+  });
+
   // //using a custom promise
-  // loadPage(req.url === '/' ? './index.html' : `.${req.url}.html`)
+  // readFilePromise(filePath)
   //   .then(data => {
   //     res.writeHead(200, { 'Content-Type': 'text/html' });
-  //     res.write(data);
-  //     return res.end
+  //     return res.end(data)
   //   })
   //   .catch(error => {
   //     console.log(error);
-  //     res.end
+  //     res.end(errorMsg)
   //   });
 
-  // // using a built in promise from fs promises
+  // // using a promise from fs promises
   // readFile(filePath, 'utf-8')
   //   .then(data => {
   //     res.writeHead(200, { 'Content-Type': 'text/html' });
-  //     res.write(data);
-  //     return res.end
+  //     res.end(data);
   //   })
   //   .catch(error => {
-
   //     console.log(error);
-
-  //     res.end(`
-  //   <h1>Oops!</h1>
-  //   <p>We can't seem to find the page you are looking for</p>
-  //   <a href="/">back home</a>
-  //   `)
+  //     res.end(errorMsg)
   //   });
-
-  //using one liner async await func
-  startLoad(req, res, filePath);
-
-  res.end
-
-  // res.end(`
-  //   <h1>Oops!</h1>
-  //   <p>We can't seem to find the page you are looking for</p>
-  //   <a href="/">back home</a>
-  //   `)
 })
 
 server.listen(5000)
