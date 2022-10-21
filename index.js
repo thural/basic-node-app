e5cd76e67565fabb50d424ba7f3704168980b0ef
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-21 12:57:37 +0400

    add get funtion for nested document query

--- index.js
+++ index.js
@@ -2,12 +2,48 @@ const express = require('express');
 const app = express();
 
 const users = [
-  {name: "John", age:33},
-  {name: "Lily", age:30}
+  {
+    id: 0,
+    name: "John",
+    age: 33,
+    comments: [
+      {
+        date: 01022020,
+        postID: 316751,
+        text: "lorem ipsum ..."
+      }
+    ]
+  },
+  {
+    id: 1,
+    name: "Lily",
+    age: 30,
+    comments: [
+      {
+        date: 03042020,
+        postID: 314713,
+        text: "lorem ipsum ..."
+      }
+    ]
+  }
 ]
 
-app.get('/', (req, res) => {
-  res.json(users);
+app.use(express.static('./public'))
+
+app.get('/users/:userID', (request, response) => {
+  const { userID } = request.params
+  const document = users.find(user => user.id == +userID)
+  if (!document) return response.status(404).send('error, document not found')
+  response.json(document);
+})
+
+app.get('/users/:userID/comments/:postID', (request, response) => {
+  const { userID, postID } = request.params
+  const user = users.find(elem => elem.id == +userID)
+  const comment = user.comments.find(elem => elem.postID == +postID)
+  console.log(comment)
+  if (!comment) return response.status(404).send('error, comment not found')
+  response.json(comment);
 })
 
 app.listen(5000)
