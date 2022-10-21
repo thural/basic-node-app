379b6564f792d26b626810e331a86f73073949e4
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-21 20:52:52 +0400

    add proper status code for responses

--- index.js
+++ index.js
@@ -7,16 +7,16 @@ app.use(express.static('./public'))
 // Get a collection using a hardcoded directory
 app.get('/api', (request, response) => {
   const collections = db;
-  if (!collections) return response.status(404).send('Error, collections not found')
-  response.json(collections);
+  if (!collections) return response.status(200).send('Error, collections not found')
+  response.status(200).json(collections);
 })
 
 // Get a collection dynamically using :param
 app.get('/api/:collection', (request, response) => {
   const { collection } = request.params;
   const data = db[collection];
-  if (!data) return response.status(404).send('Error, collection not found')
-  response.json(data);
+  if (!data) return response.status(200).json({success:true, data: []})
+  response.status(200).json(data);
 })
 
 // Get documents using nested directory and :params
@@ -24,8 +24,8 @@ app.get('/api/users/:userID/comments/:commentID', (request, response) => {
   const { userID, commentID } = request.params
   const user = db.users.find(elem => elem.id == +userID)
   const comment = user.comments.find(elem => elem.commentID == +commentID)
-  if (!comment) return response.status(404).send('Error, comment not found')
-  response.json(comment);
+  if (!comment) return response.status(200).json({success:true, data: []})
+  response.status(200).json(comment);
 })
 
 // Get data using a query?
@@ -35,7 +35,8 @@ app.get('/api/:collection/query?', (request, response) => {
   const documents = db[collection].filter(elem => {
     return (Object.keys(query)).every(tag => query[tag] == elem[tag])
   });
-  response.json(documents);
+  if(!documents) return response.status(200).json({success:true, data: []});
+  response.status(200).json(documents);
 })
 
 app.all('*', (req, res) => {
