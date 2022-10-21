a5c650a7225c06bcb31e81e0858a87e4145c0f66
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-21 23:56:06 +0400

    create basic middleware

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const { logger, authorize } = require('./middleware');
 const { db } = require('./database');
 const express = require('express');
 const app = express();
 
 app.use(express.static('./public'))
+
+// use a middleware on all routes
+app.use(logger);
+
+// use a middleware on a route
+app.use('/api/users', authorize);
 
 // Get a collection using a hardcoded directory
 app.get('/api', (request, response) => {
@@ -11,31 +18,31 @@ app.get('/api', (request, response) => {
   response.status(200).json(collections);
 })
 
+// Get documents using nested directory and :params
+app.get('/api/users/:name/comments/:commentID', (request, response) => {
+  const { name, commentID } = request.params;
+  const user = db.users.find(elem => elem["name"] == name);
+  const comment = user.comments.find(elem => elem.commentID == +commentID);
+  if (!comment) return response.status(200).json({ success: true, data: [] });
+  response.status(200).json(comment);
+})
+
 // Get a collection dynamically using :param
 app.get('/api/:collection', (request, response) => {
   const { collection } = request.params;
   const data = db[collection];
-  if (!data) return response.status(200).json({success:true, data: []})
+  if (!data) return response.status(200).json({ success: true, data: [] })
   response.status(200).json(data);
 })
 
-// Get documents using nested directory and :params
-app.get('/api/users/:userID/comments/:commentID', (request, response) => {
-  const { userID, commentID } = request.params
-  const user = db.users.find(elem => elem.id == +userID)
-  const comment = user.comments.find(elem => elem.commentID == +commentID)
-  if (!comment) return response.status(200).json({success:true, data: []})
-  response.status(200).json(comment);
-})
-
-// Get data using a query?
+// Get data using a query
 app.get('/api/:collection/query?', (request, response) => {
   const query = request.query;
   const { collection } = request.params;
   const documents = db[collection].filter(elem => {
     return (Object.keys(query)).every(tag => query[tag] == elem[tag])
   });
-  if(!documents) return response.status(200).json({success:true, data: []});
+  if (!documents.length) return response.status(200).json({ success: true, data: [] });
   response.status(200).json(documents);
 })
 
---
+++ middleware.js
@@ -0,0 +1,24 @@
+const logger = (request, response, next) => {
+  console.log(
+    request.url,
+    request.method,
+    new Date().getFullYear()
+  );
+  next()
+};
+
+const authorize = (request, response, next) => {
+  const { name } = request.query;
+  console.log('name: ', name, 'random ', Math.random());
+  if (name === "John" || name === "Lily") {
+    request.user = { "name": "John", "id": 1 }
+    next()
+  } else {
+    response.status(401).send("Unauthorized")
+  }
+};
+
+module.exports = {
+  logger,
+  authorize
+}
