9fbb5b77d298e4ce903ee3d2ec8621e28dd7c895
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-22 22:00:27 +0400

    correct status code for form post success

--- middleware.js
+++ middleware.js
@@ -14,7 +14,7 @@ const authorize = (request, response, next) => {
   const userDocument = db.users.find(user => user["name"] == name);
   if(name && password) {
     if (userDocument["password"] == password) {
-      response.status(200).send(`Welcome ${name}`);
+      response.status(201).send(`Welcome ${name}`);
       next()
     } else return response.status(401).send("Incorrect password")
   } else response.status(401).send("Incorrect login")
