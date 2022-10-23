868d71c58c153f81e9176b978350b997d3a13689
Thural Musaibov <tural.musaibov@outlook.com>
2022-10-23 12:36:52 +0400

    link query page to login post method

--- middleware.js
+++ middleware.js
@@ -14,7 +14,21 @@ const authorize = (request, response, next) => {
   const userDocument = db.users.find(user => user["name"] == name);
   if(name && password) {
     if (userDocument["password"] == password) {
-      response.status(201).send(`Welcome ${name}`);
+      //response.status(201).send(`Welcome ${name}`);
+      const options = {
+        root: __dirname + "/public",
+        dotfiles: 'deny',
+        headers: {
+          'x-timestamp': Date.now(),
+          'x-sent': true
+        }
+      }
+    
+      const fileName = "query.html";
+      response.sendFile(fileName, options, err => {
+        if (err) next(err)
+        else console.log('Sent:', fileName)
+      })
       next()
     } else return response.status(401).send("Incorrect password")
   } else response.status(401).send("Incorrect login")
